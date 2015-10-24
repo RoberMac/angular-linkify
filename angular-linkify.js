b73c93b6cd02d49115125819a4a2ee59e6daab14
RoberMac <abc269513055@gmail.com>
2015-10-24 16:29:34 +0800

    －修復：微博 hashtag 中存在「微博表情」的情況下的 BUG

--- angular-linkify.js
+++ angular-linkify.js
@@ -7,7 +7,7 @@ angular.module('linkify', [])
           return;
         }
 
-        var _text = _str.replace( /(?:https?\:\/\/)+(?![^\s]*?")([\w.,@?!^=%&amp;:\/~+#-]*[\w@?!^=%&amp;\/~+#-])?/ig, function(url) {
+        var _text = _str.replace(/(?:https?\:\/\/)+(?![^\s]*?")([\w.,@?!^=%&amp;:\/~+#-]*[\w@?!^=%&amp;\/~+#-])?/ig, function(url) {
           var wrap = document.createElement('div');
           var anch = document.createElement('a');
           anch.href = url;
@@ -35,13 +35,11 @@ angular.module('linkify', [])
           _text = _text.replace(/(^|\s)*[#＃]([^#＃\s!@$%^&*()+\-=\[\]{};':"\\|,.<>\/?\u3002\uff1f\uff01\uff0c\u3001\uff1b\uff1a\u300c\u300d\u300e\u300f\u2018\u2019\u201c\u201D\uff08\uff09\u3014\u3015\u3010\u3011\u2014\u2026\u2013\uff0e\u300a\u300B\u3008\u3009]+)/g, '$1<a href="https://instagram.com/explore/tags/$2" target="_blank">#$2</a>');
         }
 
-
         // Weibo
         if (type === 'weibo') {
           _text = _text.replace(/(|\s)*@([\u4e00-\u9fa5\w-]+)/g, '$1<a href="http://weibo.com/n/$2" target="_blank">@$2</a>');
-          _text = _text.replace(/\[[\u4e00-\u9fa5]+\]/, '').replace(/(^|\s)*#([^#]+)#/g, '$1<a href="http://huati.weibo.com/k/$2" target="_blank">#$2#</a>');
+          _text = _text.replace(/(^|\s)*#([^#]+)#/g, '$1<a href="http://huati.weibo.com/k/$2" target="_blank">#$2#</a>').replace(/\[[\u4e00-\u9fa5]*\]/g, '');
         }
-
 
         return _text;
       }
